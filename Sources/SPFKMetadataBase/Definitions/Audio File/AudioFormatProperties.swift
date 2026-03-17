c42553695c1fa627a5fe99d129e6660c94fa4f15
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-03-16 17:06:08 -0700

    cleanup

--- Sources/SPFKMetadataBase/Definitions/Audio File/AudioFormatProperties.swift
+++ Sources/SPFKMetadataBase/Definitions/Audio File/AudioFormatProperties.swift
@@ -1,4 +1,4 @@
-// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-audio
+// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi
 
 import AVFoundation
 import Foundation
